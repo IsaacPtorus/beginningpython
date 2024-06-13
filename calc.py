74a6213f83373921d8d2ed5e5cfbcd005d0ac2c8
IsaacPtorus <ptorusisaac5@gmail.com>
2024-06-13 10:39:45 +0300

    initial commit

--- calc.py
+++ calc.py
@@ -1,1 +1,49 @@
 # A simple calculator program
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero is impossible."
+    else:
+        return x / y
+
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+while True:
+    choice = input("Enter operation choice: ")
+
+    if choice in ('addition', 'subtraction', 'multiplication', 'division'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == 'addition':
+            print("Result:", add(num1, num2))
+        elif choice == 'subtraction':
+            print("Result:", subtract(num1, num2))
+        elif choice == 'multiplication':
+            print("Result:", multiply(num1, num2))
+        elif choice == 'division':
+            print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input")
+
+#
+# add = lambda x, y: x + y
+# result = add(3, 4)
+# print(result)  # Output: 7
---
+++ loops.py
@@ -0,0 +1,44 @@
+# While loop
+# 1.Increment
+number = 20
+while number <= 25:
+    print(number)
+    number += 1
+
+num = 105
+while num <= 110:
+    print("Number is", num)
+    num += 1
+
+# 2.Decrement
+count = 10
+while count >= 1:
+    print(count)
+    count -= 1
+
+# Break and continue statements
+x = 25
+while x <= 30:
+    print(x)
+    if x == 27:
+        break
+    x += 1
+
+b = 0
+while b <= 5:
+    b += 1
+    if b == 3:
+        continue
+    print(b)
+
+# For loop
+for y in range(40, 45):
+    print(y)
+for z in range(100, 110, 3):
+    print(z)
+for mynum in range(100, 110, 3):
+    print(mynum)
+
+languages = ["Python", "PHP", "Kotlin", "Java"]
+for lang in languages:
+    print(lang)
